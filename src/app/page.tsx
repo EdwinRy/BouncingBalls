9b0de4275a801c14b93624ddb01568ad5a35f9b5
EdwinRy <edwinkry@gmail.com>
2024-02-28 21:21:37 +0000

    handle no webgl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
+"use client";
+import { useEffect, useState } from 'react';
 import { BallPit } from './ui/BallSimulation/BallPit';
+import WebGL from "three/examples/jsm/capabilities/WebGL.js"
 
 export default function Home() {
     const useBrowserWindow = true;
     const padding = useBrowserWindow ? '' : 'md:p-20 p-3';
 
+    const [hasWebGL, setHasWebGL] = useState(false);
+
+    useEffect(() => {
+        setHasWebGL(WebGL.isWebGLAvailable());
+    }, []);
+
     return (
         <div className={`w-full h-full relative flex items-center justify-center ${padding}`}>
-            <BallPit />
+            {hasWebGL && <BallPit />}
+            {!hasWebGL &&
+                <div className='text-3xl'>
+                    <div>Loading WebGL...</div>
+                    <div>If this message persists, your browser may not support WebGL.</div>
+                </div>
+            }
         </div>
     );
 }
